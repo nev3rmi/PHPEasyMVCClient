dcd4f0a2eebdeb1d60d0612539ae6898d3400397
Nguyễn Văn Hải <hainvk53@gmail.com>
2017-10-08 10:30:04 +0700

    :) implement ganntchart + angular + select2

---
+++ dev/lab/exttest/jsgantt/demo/types/types.js
@@ -0,0 +1,43 @@
+angular.module("demo").controller("TypesDemoController", function($scope) {
+
+    $scope.lists = [
+        {
+            label: "Men",
+            allowedTypes: ['man'],
+            max: 4,
+            people: [
+                {name: "Bob", type: "man", resource : "Fank"},
+                {name: "Charlie", type: "man", resource : "Fank"},
+                {name: "Dave", type: "man", resource : "Fank"}
+            ]
+        },
+        {
+            label: "Women",
+            allowedTypes: ['woman'],
+            max: 4,
+            people: [
+                {name: "Alice", type: "woman", resource : "Alice"},
+                {name: "Eve", type: "woman", resource : "Alice"},
+                {name: "Peggy", type: "woman", resource : "Alice"}
+            ]
+        },
+        {
+            label: "People",
+            allowedTypes: ['man', 'woman'],
+            max: 6,
+            people: [
+                {name: "Frank", type: "man", resource : "Fank"},
+                {name: "Mallory", type: "woman", resource : "Alice"},
+                {name: "Alex", type: "unknown", resource : "Unkno"},
+                {name: "Oscar", type: "man", resource : "Fank"},
+                {name: "Wendy", type: "woman", resource : "Alice"}
+            ]
+        }
+    ];
+
+    // Model to JSON for demo purpose
+    $scope.$watch('lists', function(lists) {
+        $scope.modelAsJson = angular.toJson(lists, true);
+    }, true);
+
+});
